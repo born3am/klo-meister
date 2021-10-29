4511dcc96312327c22e10c81ba896d3a72a8f258
born3am <psmaranhao@gmail.com>
2021-10-29 16:26:37 +0200

    background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { NavLink, BrowserRouter, Route, Switch } from "react-router-dom"
+
+import DarkRoom from './Components/DarkRoom'
+import Door from './Components/Door'
 import Room from './Components/Room'
+
+import door from './img/door.png'
+import wc from './img/toilet.jpg'
+import lightButton from './img/lightButton.jpg'
 
 export default function App() {
     return (
-        <div >
-            <Room />
-        </div>
+        <BrowserRouter>
+
+            <div>
+                <img className='wc'  src={wc} alt="" />                
+                <div className='doorCombo'>
+                    <img className='door' src={door} alt="" />
+                    <img className='lightButton' src={lightButton} alt="" />
+                </div>
+               
+                <Switch>
+                    <Door />
+                    <DarkRoom />
+                    <Room />
+                </Switch>
+            </div>
+        </BrowserRouter>
     )
 }
--- src/Components/Room.js
+++ src/Components/Room.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react'
 
+import waldo from '../img/waldo-toilet.png'
+
 
 export default class Room extends Component {
-    
-    state ={
+
+    state = {
         lightOn: false
     };
 
-     lightChange =() => {
+    lightChange = () => {
         if (this.state.lightOn) {
-            this.setState ({lightOn: false});
-         }else {
-            this.setState ({lightOn: true});
-         }
+            this.setState({ lightOn: false });
+        } else {
+            this.setState({ lightOn: true });
+        }
     };
+
+    whereIsWaldo = () => {
+        if (this.state.lightOn) {
+            <img src="waldo" alt="" />
+        }
+
+    }
+
 
     render() {
         return (
-            <div className={this.state.lightOn ? "lit": " dark"} >
-                <button onClick={this.lightChange} >The bathroom switch is {this.state.lightOn?"On": "Off"}</button>
+            <div className={this.state.lightOn ? "lit" : " dark"} >
+                <button onClick={this.lightChange} >The bathroom switch is {this.state.lightOn ? "On" : "Off"}</button>
             </div>
         )
     }
 }
- 
