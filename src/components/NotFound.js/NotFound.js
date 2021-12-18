1056cbe315d4114988f6471cfc71816f6308972e
born3am <psmaranhao@gmail.com>
2021-12-18 17:56:36 +0100

    update

--- src/components/NotFound.js/NotFound.js
+++ src/components/NotFound.js/NotFound.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import {useLocation} from "react-router-dom"
-
+import './NotFound.css'
 export default function NotFound() {
     let location = useLocation();
 
     return (
-        <div>
-      <h1>
-        Resource not found at {location.pathname}
-      </h1>
+        <div className='notfound'>
             <img src="https://img.freepik.com/free-vector/404-error-with-character-error-design-template-website_114341-24.jpg?size=626&ext=jpg&ga=GA1.2.1853385046.1635638400" alt="" />
 
         </div>
