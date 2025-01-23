5e722d9454875a8caa494dda3e7aa379f0f92ce1
Pedró Sìlvá <psmaranhao@gmail.com>
2025-01-23 15:07:50 +0100

    chore(config): update base path format in vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/klo-meister/',
+  base: "/klo-meister/",
   build: {
     outDir: "dist",
   },
